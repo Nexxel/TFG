8544fc5e8c3abcaa3f98d8575cd134c67b2d2c98
Nexxel <nexel97@gmail.com>
2019-03-08 11:21:59 +0100

    Debuging width

--- learning/src/learning.cpp
+++ learning/src/learning.cpp
@@ -578,6 +578,8 @@ void getObjectPosition(int max_u, int max_v, int min_u, int min_v){
 
     double width = real_pos_max[0][0] - real_pos_min[0][0];
     
+    ROS_INFO("\n\nwidth: %.10f\n", width);
+
     robot_state.distance_c = (f/1000 * OBJECT_WIDTH) / width;
 
     // Get the pixel position in x,y
